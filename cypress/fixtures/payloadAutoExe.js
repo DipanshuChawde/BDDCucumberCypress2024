35e6929a1aaf483d1bd09d23df84133504084be3
DipanshuChawde <dipanshuchoude@gmail.com>
2024-06-14 22:48:55 +0530

    14june

---
+++ cypress/fixtures/payloadAutoExe.js
@@ -0,0 +1,40 @@
+export default [ {
+    username : "dipanshu",
+    email : `dipanshuC${Math.floor(Math.random()*10000)}@gamil.com`,
+    title : 'Mrs',
+    password : 'dipanshu',
+    dd : 26,
+    mm : 'June',
+    yy : "2000",
+    firstname : 'dipanshu',
+    lastname : 'chawde',
+    company : 'Minskole',
+    address1 : 'pune1',
+    address2 : 'pune2',
+    country : 'India',
+    state : 'maharashtra',
+    city : 'Pune',
+    zipcode : 411048,
+    mobno : 9922447802
+},
+
+{
+    username : "tanish",
+    email : `tanishC${Math.floor(Math.random()*10000)}@gamil.com`,
+    title : 'Mr',
+    password : 'tanish',
+    dd : 26,
+    mm : 'June',
+    yy : "2000",
+    firstname : 'tanish',
+    lastname : 'chawde',
+    company : 'football',
+    address1 : 'pune1',
+    address2 : 'pune2',
+    country : 'India',
+    state : 'maharashtra',
+    city : 'Pune',
+    zipcode : 411048,
+    mobno : 9922447801
+}
+]
